c84f5b7426e9abfb461aa888cc46dbb02ea9993c
to-the-galaxy <michael.thumand@gmail.com>
2022-08-26 22:41:01 +0200

    Create k8s_ubuntu22-04.sh

---
+++ scripts/k8s_ubuntu22-04.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+sudo -i
+apt update
+apt remove docker docker.io containerd runc
+apt install apt-transport-https ca-certificates curl -y
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+apt update
+apt install docker-ce docker-ce-cli containerd.io -y
+apt install -qq -y kubeadm=1.24.0-00 kubelet=1.24.0-00 kubectl=1.24.0-00
+apt-mark hold kubelet kubeadm kubectl
+
+mkdir -p /etc/containerd
+containerd config default > /etc/containerd/config.toml
+sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+{
+  cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+  overlay
+  br_netfilter
+  EOF
+}
+# vim /etc/containerd/config.toml # remove cri from disabled plugins, if listed
+systemctl restart containerd
+systemctl enable containerd
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+systemctl disable --now ufw
+{
+cat >>/etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+EOF
+}
+sysctl --system 
+
+kubeadm init \
+  --control-plane-endpoint="192.168.56.101:6443" \
+  --apiserver-advertise-address=192.168.56.101 \
+  --pod-network-cidr=10.244.0.0/16
+  
+kubectl --kubeconfig=/etc/kubernetes/admin.conf taint nodes --all node-role.kubernetes.io/control-plane- node-role.kubernetes.io/master-
+
+kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://docs.projectcalico.org/v3.18/manifests/calico.yaml
+
+kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.4/config/manifests/metallb-native.yaml
+
+cat <<EOF > metallb-settings.yaml
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: first-pool
+  namespace: metallb-system
+spec:
+  addresses:
+  - 192.168.56.200-192.168.56.210
+---
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: example
+  namespace: metallb-system
+spec:
+  ipAddressPools:
+  - first-pool
+EOF
+kubectl --kubeconfig=/etc/kubernetes/admin.conf apply -f metallb-settings.yaml
