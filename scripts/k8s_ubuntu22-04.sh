3fd610921f4721f1b47807a7780455a7591f3080
to-the-galaxy <michael.thumand@gmail.com>
2022-08-26 23:41:57 +0200

    update

--- scripts/k8s_ubuntu22-04.sh
+++ scripts/k8s_ubuntu22-04.sh
@@ -8,8 +8,8 @@ echo "Task ===> remove old versions of docker, containerd, and runc"
 apt remove docker docker.io containerd runc
 apt install apt-transport-https ca-certificates curl -y
 
-echo "Task ===> add repos"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "Task ===> add repos"  # add a -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
 
